d262d6a22143d64ce1ea724151deaa407eea093c
Erik Peldan <erik.peldan@gmail.com>
2018-07-03 21:59:40 +0200

    Identify some different types of fonts

--- page.py
+++ page.py
@@ -19,6 +19,29 @@ def transform_blocks(page):
     for b in page:
         blockm.transform(b)
 
+
+def identify_fonts(page):
+    fonts = page.findall('.//*font')
+    for f in fonts:
+        name = f.attrib['name']
+        size = f.attrib['size']
+        f.attrib = {'type': identify_font(name, size)}
+
+def identify_font(name, size):
+    if name == 'CMR10':
+        return 'regular'
+    elif name == 'CMTI10':
+        return 'italic'
+    elif name == 'CMTI8':
+        return 'reference'
+    elif name == 'CMR8':
+        return 'small'
+    elif name == 'CMR7':
+        return 'x-small'
+    else:
+        return name + "-" + str(size)
+        
 def transform(page):
     concatenate_chars(page)
     transform_blocks(page)
+    identify_fonts(page)
