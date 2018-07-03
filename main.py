1b65aedca832aff2ffdf8efcce4a3ced73404fc2
Erik Peldan <erik.peldan@gmail.com>
2018-07-03 18:41:39 +0200

    Call mutool, parse xml

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-
+import subprocess
+import tempfile
+from xml.dom import minidom
 
 def main():
-    print("HELLO WORLD")
+    parse("/home/erik/Downloads/halmos.pdf")
+
+
+def parse(filename):
+    args = ["mutool", "draw", "-X", "-Fstext", filename]
+    with tempfile.TemporaryFile() as tmp:
+        subprocess.call(args, stdout=tmp)
+        tmp.seek(0, 0)
+        xmldoc = minidom.parse(tmp)
+        print(xmldoc)
+
 
 if __name__ == "__main__":
     main()
