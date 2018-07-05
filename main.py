b896ed00ec1f7c96c1ae1a0456648f0ac25f9f80
Erik Peldan <erik.peldan@gmail.com>
2018-07-05 22:37:19 +0200

    Implement a util for just outputting the xml

--- main.py
+++ main.py
@@ -7,12 +7,27 @@ import document as doc
 
 def print_usage():
     print("Usage: epubify [input.pdf] [page(s)]?")
+    print("OR")
+    print("Usage: epubify xml [input.pdf] [page(s)]?")
     print("Example: epubify my.pdf 3-4")
+
+def xml_mode():
+    args = sys.argv[2:]
+    if len(args) == 2:
+        [filename, pages] = args
+    else:
+        pages = None
+        [filename] = args
+    xmlout('output.xml', filename, pages)
+
 
 def main():
     if len(sys.argv) < 2:
         print_usage()
         sys.exit(1)
+    if sys.argv[1] == "xml":
+        xml_mode()
+        return
     if len(sys.argv) == 3:
         [_, filename, pages] = sys.argv
     else:
@@ -38,6 +53,14 @@ def parse(filename, pages):
         tmp.seek(0, 0)
         tree = ET.parse(tmp)
         return tree.getroot()
+
+
+def xmlout(output, filename, pages):
+    args = ["mutool", "draw", "-X", "-Fstext", filename]
+    if pages:
+        args.append(str(pages))
+    with open(output, "w") as f:
+        subprocess.call(args, stdout=f)
 
 
 if __name__ == "__main__":
