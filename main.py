613069bb4f9aeea841b56f84b86c380bf41a5edc
Erik Peldan <erik.peldan@gmail.com>
2018-07-05 19:08:12 +0200

    Allow passing in page range on command line

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ import xml.etree.ElementTree as ET
 import document as doc
 
 def print_usage():
-    print("Usage: epubify [input.pdf]")
+    print("Usage: epubify [input.pdf] [page(s)]?")
+    print("Example: epubify my.pdf 3-4")
 
 def main():
     if len(sys.argv) < 2:
         print_usage()
         sys.exit(1)
-    filename = sys.argv[1]
-    document = parse(filename)
+    if len(sys.argv) == 3:
+        [_, filename, pages] = sys.argv
+    else:
+        pages = None
+        [_, filename] = sys.argv
+    document = parse(filename, pages)
     document2 = doc.transform(document)
     # print("-------------------START---------------")
     ET.ElementTree(document2).write('index.html',
@@ -24,8 +29,10 @@ def main():
     # ET.dump(document2)
 
 
-def parse(filename):
+def parse(filename, pages):
     args = ["mutool", "draw", "-X", "-Fstext", filename]
+    if pages:
+        args.append(str(pages))
     with tempfile.TemporaryFile() as tmp:
         subprocess.call(args, stdout=tmp)
         tmp.seek(0, 0)
